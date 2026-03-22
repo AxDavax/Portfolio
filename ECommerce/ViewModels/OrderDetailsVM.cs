e86e9624d3607128bf6e68e93cd6234345574fb4
axel <axdavax93@gmail.com>
2026-03-22 16:57:53 -0400

    OrderDetails: VM done + view updated

---
+++ ECommerce/ViewModels/OrderDetailsVM.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+using Portfolio.ECommerce.Blazor.Data;
+using Portfolio.ECommerce.Blazor.Repository.IRepository;
+using Portfolio.ECommerce.Blazor.Services.Extensions;
+
+namespace Portfolio.ECommerce.Blazor.ViewModels
+{
+    public class OrderDetailsVM : BaseVM
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IJSRuntime _js;
+        private readonly AuthenticationStateProvider _authenticationStateProvider;
+
+        public OrderDetailsVM(IOrderRepository orderRepository, IJSRuntime js, AuthenticationStateProvider authenticationStateProvider)
+        {
+            _orderRepository = orderRepository;
+            _js = js;
+            _authenticationStateProvider = authenticationStateProvider;
+        }
+
+        private bool _isProcessing = true;
+        public bool IsProcessing
+        {
+            get => _isProcessing;
+            set => SetProperty(ref _isProcessing, value);
+        }
+
+        private int _id;
+        public int Id
+        {
+            get => _id;
+            set => SetProperty(ref _id, value);
+        }
+
+        private OrderHeader? _orderHeader = null;
+        public OrderHeader? OrderHeader
+        {
+            get => _orderHeader;
+            set => SetProperty(ref _orderHeader, value);
+        }
+
+        public Task InitializeAsync() => Task.CompletedTask;
+
+        public async Task AfterRenderAsync(bool firstRender)
+        {
+            if (firstRender) await LoadOrderAsync();
+        }
+
+        public async Task LoadOrderAsync()
+        {
+            await RunCommandAsync(() => IsProcessing, async () =>
+            {
+                OrderHeader = await _orderRepository.GetAsync(Id);
+            });
+        }
+
+        public async Task UpdateStatusAsync(string newStatus)
+        {
+            await RunCommandAsync(() => IsProcessing, async () =>
+            {
+                await _orderRepository.UpdateStatusAsync(Id, newStatus, "");
+                _js?.ToastrSuccess($"Status updated successfully to {newStatus}");
+            });
+        }
+    }
+}
