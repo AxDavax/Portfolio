6396aec5c9d3bb7571609e7d387cff28b66c7568
axel <axdavax93@gmail.com>
2026-03-28 13:12:45 -0400

    Changing GetAsync by GetByIdAsync in ICategoryRepository

--- ECommerce/ECommerce.Domain/Interfaces/ICategoryRepository.cs
+++ ECommerce/ECommerce.Domain/Interfaces/ICategoryRepository.cs
@@ -7,6 +7,6 @@ public interface ICategoryRepository
     public Task<Category> CreateAsync(Category obj);
     public Task<Category> UpdateAsync(Category obj);
     public Task<bool> DeleteAsync(int id);
-    public Task<Category> GetAsync(int id);
+    public Task<Category?> GetByIdAsync(int id);
     public Task<IEnumerable<Category>> GetAllAsync();
 }
