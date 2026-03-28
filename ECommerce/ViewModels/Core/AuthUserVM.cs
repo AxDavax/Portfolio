5c7f382131d8671b201c304ece298424e013b497
axel <axdavax93@gmail.com>
2026-03-27 20:18:17 -0400

    deleting unnecessary namespaces in AuthUserVM

--- ECommerce/ViewModels/Core/AuthUserVM.cs
+++ ECommerce/ViewModels/Core/AuthUserVM.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Server;
-using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
 
 namespace Portfolio.ECommerce.Blazor.ViewModels.Core
