6c21807a3a423a5203a0f87caa4163cab96362a8
axel <axdavax93@gmail.com>
2026-03-28 11:08:35 -0400

    Starting Infrasture project with DapperContext and SqlDataAccess which implements ISqlDataAccess

---
+++ ECommerce/ECommerce.Domain/Interfaces/ISqlDataAccess.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerce.Domain.Interfaces;
+
+public interface ISqlDataAccess
+{
+    Task<IEnumerable<T>> QueryAsync<T, U>(string sql, U parameters);
+    Task<T?> QuerySingleAsync<T, U>(string sql, U parameters);
+    Task<int> ExecuteAsync<T>(string sql, T parameters);
+    Task<T> ExecuteScalarAsync<T, U>(string sql, U parameters);
+}
---
+++ ECommerce/ECommerce.Infrastructure/Data/SqlDataAccess.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using ECommerce.Domain.Interfaces;
+using System.Data;
+
+namespace ECommerce.Infrastructure.Data;
+
+public class SqlDataAccess : ISqlDataAccess
+{
+    private readonly DapperContext _context;
+
+    public SqlDataAccess(DapperContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> ExecuteAsync<T>(string sql, T parameters)
+    {
+        using IDbConnection connection = _context.CreateConnection();
+        return await connection.ExecuteAsync(sql, parameters);
+    }
+
+    public async Task<T> ExecuteScalarAsync<T, U>(string sql, U parameters)
+    {
+        using IDbConnection connection = _context.CreateConnection();
+        return await connection.ExecuteScalarAsync<T>(sql, parameters);
+    }
+
+    public async Task<IEnumerable<T>> QueryAsync<T, U>(string sql, U parameters)
+    {
+        using IDbConnection connection = _context.CreateConnection();
+        return await connection.QueryAsync<T>(sql, parameters);
+    }
+
+    public async Task<T?> QuerySingleAsync<T, U>(string sql, U parameters)
+    {
+        using IDbConnection connection = _context.CreateConnection();
+        return await connection.QueryFirstOrDefaultAsync<T>(sql, parameters);
+    }
+}
