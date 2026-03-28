c96407b8fba1a274b406c3552b94e79bcd59f608
axel <axdavax93@gmail.com>
2026-03-28 12:54:16 -0400

    Changing QuerySingleAsync to QuerySingleOrDefaultAsync

--- ECommerce/ECommerce.Domain/Interfaces/ISqlDataAccess.cs
+++ ECommerce/ECommerce.Domain/Interfaces/ISqlDataAccess.cs
@@ -3,7 +3,7 @@
 public interface ISqlDataAccess
 {
     Task<IEnumerable<T>> QueryAsync<T, U>(string sql, U parameters);
-    Task<T?> QuerySingleAsync<T, U>(string sql, U parameters);
+    Task<T?> QuerySingleOrDefaultAsync<T, U>(string sql, U parameters);
     Task<int> ExecuteAsync<T>(string sql, T parameters);
     Task<T> ExecuteScalarAsync<T, U>(string sql, U parameters);
 }
