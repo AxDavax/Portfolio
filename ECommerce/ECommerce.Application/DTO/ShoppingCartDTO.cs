713f30cd93932f4d2e8aa58154ef45874cc8b877
axel <axdavax93@gmail.com>
2026-04-01 20:21:59 -0400

    Adding ShoppingCartDTO in DTO folder of Application Project

---
+++ ECommerce/ECommerce.Application/DTO/ShoppingCartDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Application.DTO;
+
+public class ShoppingCartDTO
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string UserId { get; set; }
+
+    [Range(1, 100)]
+    public int Count { get; set; }
+
+    public int ProductId { get; set; }
+
+    public ProductDTO? Product { get; set; }
+}
