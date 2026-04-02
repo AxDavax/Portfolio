0008c1999d1aff9d4c733e746cedc6870cf18cdf
axel <axdavax93@gmail.com>
2026-04-02 11:40:45 -0400

    Adding CartController with CartMapping static class

---
+++ ECommerce/ECommerce.API/Controllers/CartController.cs
@@ -0,0 +1,35 @@
+﻿using ECommerce.Application.DTO;
+using ECommerce.Application.Mappings;
+using ECommerce.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CartController : ControllerBase
+    {
+        private readonly ICartToOrder _cartToOrder;
+
+        public CartController(ICartToOrder cartToOrder)
+        {
+            _cartToOrder = cartToOrder;
+        }
+
+        // POST: api/cart/convert
+        [HttpPost("convert")]
+        public IActionResult ConvertCartToOrderDetails([FromBody] List<ShoppingCartDTO> carts)
+        {
+            if (carts == null || carts.Count == 0)
+                return BadRequest("Cart is empty.");
+
+            var orderDetails = _cartToOrder.ConvertShoppingCartToOrderDetails(
+                carts.Select(c => c.ToDomain()).ToList()
+            );
+
+            var result = orderDetails.Select(od => od.ToDTO()).ToList();
+
+            return Ok(result);
+        }
+    }
+}
---
+++ ECommerce/ECommerce.Application/Mappings/CartMappings.cs
@@ -0,0 +1,34 @@
+﻿using ECommerce.Application.DTO;
+using ECommerce.Domain.Models;
+
+namespace ECommerce.Application.Mappings;
+
+public static class CartMappings
+{
+    public static ShoppingCart ToDomain(this ShoppingCartDTO dto)
+    {
+        return new ShoppingCart
+        {
+            Id = dto.Id,
+            ProductId = dto.ProductId,
+            Count = dto.Count,
+            Product = new Product
+            {
+                Id = dto.Product!.Id,
+                Name = dto.Product.Name,
+                Price = dto.Product.Price
+            }
+        };
+    }
+
+    public static OrderDetailDTO ToDTO(this OrderDetail domain)
+    {
+        return new OrderDetailDTO
+        {
+            ProductId = domain.ProductId,
+            Count = domain.Count,
+            Price = domain.Price,
+            ProductName = domain.ProductName
+        };
+    }
+}
