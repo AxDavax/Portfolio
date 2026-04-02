eeec6b1803f0d1cad8ee52e63442ff6f63dbab41
axel <axdavax93@gmail.com>
2026-04-02 10:28:53 -0400

    Adding PaymentController in API project

---
+++ ECommerce/ECommerce.API/Controllers/PaymentController.cs
@@ -0,0 +1,34 @@
+﻿using ECommerce.Application.DTO;
+using ECommerce.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IPaymentService _paymentService;
+
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        // POST: api/payment/create-session
+        [HttpPost("create-session")]
+        public async Task<IActionResult> CreateCheckoutSession([FromBody] OrderHeaderDTO order)
+        {
+            var url = await _paymentService.CreateCheckoutSessionAsync(order);
+            return Ok(new { url });
+        }
+
+        // GET: api/payment/verify/{sessionId}
+        [HttpGet("verify/{sessionId}")]
+        public async Task<IActionResult> VerifyPayment(string sessionId)
+        {
+            var success = await _paymentService.VerifyPaymentAsync(sessionId);
+            return Ok(new { success });
+        }
+    }
+}
