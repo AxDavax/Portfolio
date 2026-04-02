9473fdf57ba5badf947f9db369b514d5af7cc35b
axel <axdavax93@gmail.com>
2026-04-01 21:53:19 -0400

    Renaming IOrderService by ICartToOrder and OrderService by CartToOrder

---
+++ ECommerce/ECommerce.Application/Interfaces/ICartToOrder.cs
@@ -0,0 +1,8 @@
+﻿using ECommerce.Domain.Models;
+
+namespace ECommerce.Domain.Interfaces;
+
+public interface ICartToOrder
+{
+    List<OrderDetail> ConvertShoppingCartToOrderDetails(List<ShoppingCart> carts);
+}
---
+++ ECommerce/ECommerce.Application/Services/CartToOrder.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce.Domain.Interfaces;
+using ECommerce.Domain.Models;
+
+namespace ECommerce.Application.Services;
+
+public class CartToOrder : ICartToOrder
+{
+    public List<OrderDetail> ConvertShoppingCartToOrderDetails(List<ShoppingCart> carts)
+    {
+        return carts.Select(cart => new OrderDetail
+        {
+            ProductId = cart.ProductId,
+            Count = cart.Count,
+            Price = Convert.ToDouble(cart.Product.Price),
+            ProductName = cart.Product.Name
+        }).ToList();
+    }
+}
