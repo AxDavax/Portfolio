d7b532a51d6db0a91e09c38ab75161339e728729
axel <axdavax93@gmail.com>
2026-04-02 20:21:21 -0400

    Adding new Blazor Web App project with interactivity handle by component/page

---
+++ ECommerce/ECommerce.Blazor_WebApp/ECommerce.Blazor_WebApp.Client/Services/SharedStateService.cs
@@ -0,0 +1,19 @@
+﻿namespace ECommerce.Blazor_WebApp.Client.Services
+{
+    public class SharedStateService
+    {
+        public event Action OnChange;
+        private int _totalCartCount;
+
+        public int TotalCartCount 
+        {
+            get => _totalCartCount;
+            set { 
+                _totalCartCount = value;
+                NotifyStateChanged();
+            } 
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
---
+++ ECommerce/ECommerce.Blazor_WebApp/ECommerce.Blazor_WebApp/Program.cs
@@ -0,0 +1,36 @@
+using ECommerce.Blazor_WebApp.Client.Pages;
+using ECommerce.Blazor_WebApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(ECommerce.Blazor_WebApp.Client._Imports).Assembly);
+
+app.Run();
