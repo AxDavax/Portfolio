e35dded4d61d9d51ff3dbbee93092cf615340d2b
axel <axdavax93@gmail.com>
2026-04-04 18:50:31 -0400

    Adding ViewModels folder with Core subfolder and parents viewModels

---
+++ ECommerce/ECommerce.Blazor_WebApp/ECommerce.Blazor_WebApp.Client/ViewModels/Core/AuthUserVM.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace ECommerce.Blazor_WebApp.Client.ViewModels.Core;
+
+public class AuthUserVM : BaseVM
+{
+    private readonly AuthenticationStateProvider _authStateProvider;
+
+    private ClaimsPrincipal _user;
+    public ClaimsPrincipal User 
+    { 
+        get => _user;
+        private set => SetProperty(ref _user, value); 
+    }
+
+    public bool IsReady => User != null;
+
+    public AuthUserVM(AuthenticationStateProvider authenticationStateProvider) 
+    {
+        _authStateProvider = authenticationStateProvider;
+
+        _authStateProvider.AuthenticationStateChanged += OnAuthStateChanged;
+    }
+
+    private async void OnAuthStateChanged(Task<AuthenticationState> task)
+    {
+        var authState = await task;
+        User = authState.User;
+
+        OnPropertyChanged(nameof(IsReady));
+    }
+
+    public async Task LoadAsync()
+    {
+        var authState = await _authStateProvider.GetAuthenticationStateAsync();
+        User = authState.User;
+
+        OnPropertyChanged(nameof(IsReady));
+    }
+}
