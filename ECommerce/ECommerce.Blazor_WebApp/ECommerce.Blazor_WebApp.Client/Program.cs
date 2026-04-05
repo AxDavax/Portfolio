f0426678ac41c44b47afff692eee6bf129472393
axel <axdavax93@gmail.com>
2026-04-05 14:54:19 -0400

    trying to make Home page work without succes

--- ECommerce/ECommerce.Blazor_WebApp/ECommerce.Blazor_WebApp.Client/Program.cs
+++ ECommerce/ECommerce.Blazor_WebApp/ECommerce.Blazor_WebApp.Client/Program.cs
@@ -1,12 +1,13 @@
 using ECommerce.Blazor_WebApp.Client.Services.API;
 using ECommerce.Blazor_WebApp.Client.Services.State;
+using ECommerce.Blazor_WebApp.Client.ViewModels;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddHttpClient();
 builder.Services.AddApiServices(builder.Configuration);
-
 builder.Services.AddScoped<SharedStateService>();
+builder.Services.AddViewModels();
 builder.Services.AddAuthorizationCore();
 await builder.Build().RunAsync();
---
+++ ECommerce/ECommerce.Blazor_WebApp/ECommerce.Blazor_WebApp.Client/ViewModels/VMsConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.Blazor_WebApp.Client.ViewModels.Core;
+using ECommerce.Blazor_WebApp.Client.ViewModels.Home;
+
+namespace ECommerce.Blazor_WebApp.Client.ViewModels;
+
+public static class VMsConfiguration
+{
+    public static void AddViewModels(this IServiceCollection services)
+    {
+        services.AddScoped<AuthUserVM>();
+        services.AddScoped<HomeVM>();
+    }
+}
