bb56423316c5a8e5238d3e9b7ec564ba4f200ee0
axel <axdavax93@gmail.com>
2026-04-06 14:51:46 -0400

    Registering Application's services in his registering class

--- ECommerce/ECommerce.Application/DependencyInjection.cs
+++ ECommerce/ECommerce.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using ECommerce.Application.UseCases.Auth.Login;
+﻿using ECommerce.Application.Interfaces;
+using ECommerce.Application.Services;
+using ECommerce.Application.UseCases.Auth.Login;
 using ECommerce.Application.UseCases.Auth.Me;
 using ECommerce.Application.UseCases.Auth.Refresh;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,9 +11,17 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        // Auth Use Cases
         services.AddScoped<LoginHandler>();
         services.AddScoped<RefreshHandler>();
         services.AddScoped<MeHandler>();
+
+        // Services
+        services.AddScoped<ICartToOrder, CartToOrder>();
+        services.AddScoped<ICategoryService, CategoryService>();
+        services.AddScoped<IProductService, ProductService>();
+        services.AddScoped<IShoppingCartService, ShoppingCartService>();
+        services.AddScoped<IOrderService, OrderService>();
 
         return services;
     }
