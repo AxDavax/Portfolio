cc1c9e5224aab5aef6637774943d13a0c0d87299
axel <axdavax93@gmail.com>
2026-04-06 14:54:36 -0400

    Adding Registering DI class in Infrastructure project

---
+++ ECommerce/ECommerce.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,37 @@
+﻿using ECommerce.Application.Interfaces;
+using ECommerce.Domain.Interfaces;
+using ECommerce.Infrastructure.Auth;
+using ECommerce.Infrastructure.Data;
+using ECommerce.Infrastructure.Repositories;
+using ECommerce.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerce.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        // Data
+        services.AddSingleton<DapperContext>();
+        services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+
+        // Repositories
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        // Auth 
+        services.AddSingleton<IJwtService, JwtService>();
+        services.AddScoped<IRefreshTokenService, RefreshTokenService>();
+
+        // Services
+        services.AddScoped<IFileService, FileService>();
+        services.AddSingleton<IPasswordService, PasswordService>();
+        services.AddScoped<IPaymentService, PaymentService>();
+        
+        return services;
+    }
+}
