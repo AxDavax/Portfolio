e6d6c84092f1d172c77e5ac3fce084f179f8f7c8
axel <axdavax93@gmail.com>
2026-04-05 22:53:11 -0400

    Adding IPasswordService and PasswordService in Backend project to use in Authentication

---
+++ ECommerce/ECommerce.Application/Interfaces/IPasswordService.cs
@@ -0,0 +1,7 @@
+﻿namespace ECommerce.Application.Interfaces;
+
+public interface IPasswordService
+{
+    (string Hash, string Salt) HashPassword(string password);
+    bool VerifyPassword(string password, string hash, string salt);
+}
---
+++ ECommerce/ECommerce.Infrastructure/Services/PasswordService.cs
@@ -0,0 +1,34 @@
+﻿using ECommerce.Application.Interfaces;
+using System.Security.Cryptography;
+
+namespace ECommerce.Infrastructure.Services;
+
+public class PasswordService : IPasswordService
+{
+    private const int SaltSize = 16;
+    private const int KeySize = 32;
+    private const int Iterations = 100000;
+
+    public (string Hash, string Salt) HashPassword(string password)
+    {
+        using var rng = RandomNumberGenerator.Create();
+        byte[] saltBytes = new byte[SaltSize];
+        rng.GetBytes(saltBytes);
+
+        using var pbkdf2 = new Rfc2898DeriveBytes(password, saltBytes, Iterations, HashAlgorithmName.SHA256);
+        byte[] hashBytes = pbkdf2.GetBytes(KeySize);
+
+        return (Convert.ToBase64String(hashBytes), Convert.ToBase64String(saltBytes));
+    }
+
+    public bool VerifyPassword(string password, string hash, string salt)
+    {
+        byte[] saltBytes = Convert.FromBase64String(salt);
+        byte[] hashBytes = Convert.FromBase64String(hash);
+
+        using var pbkdf2 = new Rfc2898DeriveBytes(password, saltBytes, Iterations, HashAlgorithmName.SHA256);
+        byte[] computedHash = pbkdf2.GetBytes(KeySize);
+
+        return CryptographicOperations.FixedTimeEquals(hashBytes, computedHash);
+    }
+}
