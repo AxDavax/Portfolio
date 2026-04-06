2d3663a9b9e0c1c44b08f402248811f9597b8c66
axel <axdavax93@gmail.com>
2026-04-06 12:25:53 -0400

    Adding IUserAuthRepository to Application project to use SqlUser

---
+++ ECommerce/ECommerce.Application/Interfaces/IUserAuthRepository.cs
@@ -0,0 +1,8 @@
+﻿using ECommerce.Application.Models;
+
+namespace ECommerce.Application.Interfaces;
+
+public interface IUserAuthRepository
+{
+    Task<SqlUser?> GetSqlUserByEmailAsync(string email);
+}
