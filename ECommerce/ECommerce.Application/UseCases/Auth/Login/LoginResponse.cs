ecf39a016efeecc90a90301cccc20b93059a09fd
axel <axdavax93@gmail.com>
2026-04-06 12:10:10 -0400

    Starts implementing UseCases folder with Auth/Login first steps

---
+++ ECommerce/ECommerce.Application/UseCases/Auth/Login/LoginResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerce.Application.UseCases.Auth.Login;
+
+public class LoginResponse
+{
+    public string Token { get; set; } = string.Empty;
+    public string RefreshToken { get; set; } = string.Empty;
+
+    public Guid UserId { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public IEnumerable<string> Roles { get; set; } = Enumerable.Empty<string>();
+}
