7b40e6cd2155ba7afad10ad920ba04d21ef517c7
axel <axdavax93@gmail.com>
2026-04-06 13:57:28 -0400

    Adding MeRequest and MeResponse in Me Subfolder

---
+++ ECommerce/ECommerce.Application/UseCases/Auth/Me/MeRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace ECommerce.Application.UseCases.Auth.Me;
+
+public class MeRequest
+{
+    public Guid UserId { get; set; }
+}
---
+++ ECommerce/ECommerce.Application/UseCases/Auth/Me/MeResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.Application.UseCases.Auth.Me;
+
+public class MeResponse
+{
+    public Guid UserId { get; set; }
+    public string Email { get; set; }
+    public IEnumerable<string> Roles { get; set; } = Enumerable.Empty<string>();
+}
