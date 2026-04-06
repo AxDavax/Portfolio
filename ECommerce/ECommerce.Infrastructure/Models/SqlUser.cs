8f5cf61a1f9a21a5492908f44f4fa7121e932fbd
axel <axdavax93@gmail.com>
2026-04-06 01:35:36 -0400

    Adding User model and SqlUser model considered User DTO in two different projects

---
+++ ECommerce/ECommerce.Infrastructure/Models/SqlUser.cs
@@ -0,0 +1,13 @@
+﻿namespace ECommerce.Infrastructure.Models;
+
+public class SqlUser
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public string PasswordSalt { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
