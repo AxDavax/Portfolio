de4328406991824aba4b69e28904950554ce4a90
axel <axdavax93@gmail.com>
2026-04-06 15:37:52 -0400

    Adding AuthController with ClaimsPrincipalExtensions static class

---
+++ ECommerce/ECommerce.API/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using ECommerce.API.Extensions;
+using ECommerce.Application.UseCases.Auth.Login;
+using ECommerce.Application.UseCases.Auth.Me;
+using ECommerce.Application.UseCases.Auth.Refresh;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly LoginHandler _loginHandler;
+    private readonly RefreshHandler _refreshHandler;
+    private readonly MeHandler _meHandler;
+
+    public AuthController(
+        LoginHandler loginHandler, 
+        RefreshHandler refreshHandler, 
+        MeHandler meHandler)
+    {
+        _loginHandler = loginHandler;
+        _refreshHandler = refreshHandler;
+        _meHandler = meHandler;
+    }
+
+
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequest request)
+    {
+        var response = await _loginHandler.Handle(request);
+        return Ok(response);
+    }
+
+    [AllowAnonymous]
+    [HttpPost("refresh")]
+    public async Task<IActionResult> Refresh([FromBody] RefreshRequest request)
+    {
+        var response = await _refreshHandler.Handle(request);
+        return Ok(response);
+    }
+
+    [Authorize]
+    [HttpGet("me")]
+    public async Task<IActionResult> Me()
+    {
+        var userId = User.GetUserId();
+
+        var response = await _meHandler.Handle(new MeRequest
+        {
+            UserId = userId
+        });
+
+        return Ok(response);
+    }
+}
