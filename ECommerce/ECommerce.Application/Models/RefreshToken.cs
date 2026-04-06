87f62132c544a2d59d0c1b7b84b7004a68647e9d
axel <axdavax93@gmail.com>
2026-04-06 13:30:26 -0400

    Adding RefreshToken in Application project  Models folder

---
+++ ECommerce/ECommerce.Application/Models/RefreshToken.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.Application.Models;
+
+public class RefreshToken
+{
+    public Guid UserId { get; set; }
+    public string Token { get; set; } = string.Empty;
+    public DateTime ExpiryDate { get; set; }
+
+    public bool IsExpired => ExpiryDate <= DateTime.UtcNow;
+}
