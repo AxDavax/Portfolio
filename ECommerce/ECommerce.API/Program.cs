a6cdbf8523d427f37738630c31f44669c5eff617
axel <axdavax93@gmail.com>
2026-04-06 14:55:49 -0400

    Using registered DI Class from Application and Infrastructure Projects in Program.cs making it easily readable

--- ECommerce/ECommerce.API/Program.cs
+++ ECommerce/ECommerce.API/Program.cs
@@ -1,14 +1,6 @@
 using ECommerce.Application;
-using ECommerce.Application.Interfaces;
-using ECommerce.Application.Services;
-using ECommerce.Domain.Interfaces;
-using ECommerce.Infrastructure.Auth;
-using ECommerce.Infrastructure.Data;
-using ECommerce.Infrastructure.Repositories;
-using ECommerce.Infrastructure.Services;
+using ECommerce.Infrastructure;
 using Stripe;
-using FileService = ECommerce.Infrastructure.Services.FileService;
-using ProductService = ECommerce.Application.Services.ProductService;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,24 +9,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-builder.Services.AddSingleton<DapperContext>();
-builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<ICategoryService, CategoryService>();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
-builder.Services.AddScoped<IShoppingCartService, ShoppingCartService>();
-builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-builder.Services.AddScoped<IOrderService, OrderService>();
-builder.Services.AddScoped<IFileService, FileService>();
-builder.Services.AddScoped<IPaymentService, PaymentService>();
-builder.Services.AddSingleton<IPasswordService, PasswordService>();
-builder.Services.AddSingleton<IJwtService, JwtService>();
-builder.Services.AddScoped<IRefreshTokenService, RefreshTokenService>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
 
 builder.Services.AddCors(options =>
 {
