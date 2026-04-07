a3de6c630e4bdf86aff8a6c4e383688b8f88f7a1
axel <axdavax93@gmail.com>
2026-04-07 18:24:43 -0400

    Adding a new Profile for Mapping SqlUser with User

---
+++ ECommerce/ECommerce.Application/Mappings/Profiles/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ECommerce.Application.Models;
+using ECommerce.Domain.Models;
+namespace ECommerce.Application.Mappings.Profiles;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<SqlUser, User>();
+
+        CreateMap<User, SqlUser>()
+            .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+            .ForMember(dest => dest.PasswordSalt, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore());
+    }
+}
