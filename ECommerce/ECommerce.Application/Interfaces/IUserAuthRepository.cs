c4dde76d72d490fa9e45a194e3928e9b49880c02
axel <axdavax93@gmail.com>
2026-04-07 18:23:51 -0400

    Adding a new signature in IUserAuthRepository

--- ECommerce/ECommerce.Application/Interfaces/IUserAuthRepository.cs
+++ ECommerce/ECommerce.Application/Interfaces/IUserAuthRepository.cs
@@ -1,8 +1,10 @@
 ﻿using ECommerce.Application.Models;
+using ECommerce.Domain.Models;
 
 namespace ECommerce.Application.Interfaces;
 
 public interface IUserAuthRepository
 {
     Task<SqlUser?> GetSqlUserByEmailAsync(string email);
+    Task<User> CreateAuthAsync(SqlUser user);
 }
