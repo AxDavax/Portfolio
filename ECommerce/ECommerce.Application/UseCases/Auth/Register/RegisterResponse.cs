071f010734d87760fc0d7623029187e9f856c7fc
axel <axdavax93@gmail.com>
2026-04-07 17:45:35 -0400

    Adding a new Use Case for Auth subfolder

---
+++ ECommerce/ECommerce.Application/UseCases/Auth/Register/RegisterResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.Application.UseCases.Auth.Register;
+
+public class RegisterResponse
+{
+    public string Token { get; set; } = string.Empty;
+    public string RefreshToken {  get; set; } = string.Empty;
+    public DateTime Expiration {  get; set; }
+}
