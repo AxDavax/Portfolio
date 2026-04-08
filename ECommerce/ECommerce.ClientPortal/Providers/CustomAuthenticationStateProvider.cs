d2100ff8a2831d441c11b0c7713dcab539b10191
axel <axdavax93@gmail.com>
2026-04-08 00:13:00 -0400

    Adding a new CustomAuthenticationStateProvider in Providers folder

---
+++ ECommerce/ECommerce.ClientPortal/Providers/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,50 @@
+﻿using ECommerce.ClientPortal.Services.Auth;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace ECommerce.ClientPortal.Providers;
+
+public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+{
+    private readonly TokenStorageService _tokenStorage;
+
+    public CustomAuthenticationStateProvider(TokenStorageService tokenStorage)
+    {
+        _tokenStorage = tokenStorage;
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var token = await _tokenStorage.GetTokenAsync();
+
+        if (string.IsNullOrWhiteSpace(token))
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+
+        var identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+        var user = new ClaimsPrincipal(identity);
+
+        return new AuthenticationState(user);
+    }
+
+    public async Task MarkUserAsAuthenticated(string token)
+    {
+        var identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+        var user = new ClaimsPrincipal(identity);
+
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+    }
+
+    public void MarkUserAsLoggedOut()
+    {
+        var anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(anonymous)));
+    }
+
+    private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        var token = handler.ReadJwtToken(jwt);
+        return token.Claims;
+    }
+}
