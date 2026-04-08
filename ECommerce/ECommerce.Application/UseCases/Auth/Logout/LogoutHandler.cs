ea0810c869fa3dff587c159c7d512d7d61440d0b
axel <axdavax93@gmail.com>
2026-04-07 22:02:34 -0400

    Adding LogoutHandler in UseCases/Auth/Logout

---
+++ ECommerce/ECommerce.Application/UseCases/Auth/Logout/LogoutHandler.cs
@@ -0,0 +1,28 @@
+﻿using ECommerce.Application.Interfaces;
+
+namespace ECommerce.Application.UseCases.Auth.Logout
+{
+    public class LogoutHandler
+    {
+        private readonly IRefreshTokenService _refreshTokenService;
+
+        public LogoutHandler(IRefreshTokenService refreshTokenService)
+        {
+            _refreshTokenService = refreshTokenService;
+        }
+
+        public async Task<LogoutResponse> HandleAsync(LogoutRequest request)
+        {
+            // 1. Verify if the token exists
+            var existing = await _refreshTokenService.GetRefreshTokenAsync(request.RefreshToken);
+            if (existing != null)
+                return new LogoutResponse { Success = false };
+
+            // 2. Deletes the refresh token
+            await _refreshTokenService.DeleteRefreshTokenAsync(request.RefreshToken);
+
+            // 3. Returns Success
+            return new LogoutResponse { Success = true };
+        }
+    }
+}
