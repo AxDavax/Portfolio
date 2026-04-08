f147806fb77d264d8b4ae3c11d427be573c92337
axel <axdavax93@gmail.com>
2026-04-08 01:01:39 -0400

    Adding AuthService in ClientPortal project

---
+++ ECommerce/ECommerce.ClientPortal/Services/Auth/AuthService.cs
@@ -0,0 +1,65 @@
+﻿using ECommerce.ClientPortal.Providers;
+using ECommerce.Contracts.Auth.Login;
+using ECommerce.Contracts.Auth.Logout;
+using ECommerce.Contracts.Auth.Register;
+using System.Net.Http.Json;
+
+namespace ECommerce.ClientPortal.Services.Auth;
+
+public class AuthService
+{
+    private readonly HttpClient _http;
+    private readonly TokenStorageService _tokenStorage;
+    private readonly CustomAuthenticationStateProvider _authStateProvider;
+
+    public AuthService(
+        HttpClient http,
+        TokenStorageService tokenStorage,
+        CustomAuthenticationStateProvider authStateProvider)
+    {
+        _http = http;
+        _tokenStorage = tokenStorage;
+        _authStateProvider = authStateProvider;
+    }
+
+    public async Task<bool> Login(LoginRequest request)
+    {
+        var response = await _http.PostAsJsonAsync("api/auth/login", request);
+
+        if(!response.IsSuccessStatusCode) return false;
+
+        var result = await response.Content.ReadFromJsonAsync<LoginResponse>();
+
+        await _tokenStorage.SaveAsync(result.Token, result.RefreshToken, result.Expiration);
+        await _authStateProvider.MarkUserAsAuthenticated(result.Token);
+
+        return true;
+    }
+
+    public async Task<bool> Register(RegisterRequest request) 
+    {
+        var response = await _http.PostAsJsonAsync("api/auth/register", request);
+
+        if(!response.IsSuccessStatusCode) return false;
+
+        var result = await response.Content.ReadFromJsonAsync<RegisterResponse>();
+
+        await _tokenStorage.SaveAsync(result.Token, result.RefreshToken, result.Expiration);
+        await _authStateProvider.MarkUserAsAuthenticated(result.Token);
+
+        return true;
+    }
+
+    public async Task Logout()
+    {
+        var refreshToken = await _tokenStorage.GetRefreshTokenAsync();
+
+        await _http.PostAsJsonAsync("api/auth/logout", new LogoutRequest
+        {
+            RefreshToken = refreshToken!
+        });
+
+        await _tokenStorage.ClearAsync();
+        _authStateProvider.MarkUserAsLoggedOut();
+    }
+}
