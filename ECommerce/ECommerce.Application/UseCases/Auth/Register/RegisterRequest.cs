923e96cf170c8b0f9d7b982d54c724deb626d5a4
axel <axdavax93@gmail.com>
2026-04-07 21:18:06 -0400

    Updating RegisterRequest to fit RegisterHandler

--- ECommerce/ECommerce.Application/UseCases/Auth/Register/RegisterRequest.cs
+++ ECommerce/ECommerce.Application/UseCases/Auth/Register/RegisterRequest.cs
@@ -4,5 +4,6 @@ public class RegisterRequest
 {
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
 }
