bf5956b5e88042b46558010a5e1192d23685bcc2
axel <axdavax93@gmail.com>
2026-04-08 22:36:59 -0400

    Adding ProfileVM and Profile page

---
+++ ECommerce/ECommerce.ClientPortal/ViewModels/Auth/ProfileVM.cs
@@ -0,0 +1,32 @@
+﻿using ECommerce.ClientPortal.ViewModels.Core;
+using System.Security.Claims;
+
+namespace ECommerce.ClientPortal.ViewModels.Auth;
+
+public class ProfileVM : BaseVM
+{
+    private readonly AuthUserVM _authUser;
+
+    public string Email => _authUser.User?.Identity?.Name ?? string.Empty;
+    public string FirstName => _authUser.User?.FindFirst("firstName")?.Value ?? string.Empty;
+    public string LastName => _authUser.User?.FindFirst("lastname")?.Value ?? String.Empty;
+
+    public IEnumerable<Claim> Claims => _authUser.User?.Claims ?? Enumerable.Empty<Claim>();
+
+
+    public ProfileVM(AuthUserVM authUser)
+    {
+        _authUser = authUser;
+
+        // when AuthUserVM changes, ProfileVM updates automatically
+        _authUser.PropertyChanged += (_, __) => OnPropertyChanged(null);
+    }
+
+    public async Task LoadAsync()
+    {
+        if(!_authUser.IsReady)
+            await _authUser.LoadAsync();
+
+        OnPropertyChanged(null);
+    }
+}
