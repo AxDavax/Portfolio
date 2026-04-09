d1dff7d1d29d477024fab9b4accb05159c797c75
axel <axdavax93@gmail.com>
2026-04-08 21:55:15 -0400

    Adding LogoutVM and Logout page

---
+++ ECommerce/ECommerce.ClientPortal/ViewModels/Auth/LogoutVM.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.ClientPortal.Services.Auth;
+using ECommerce.ClientPortal.ViewModels.Core;
+using Microsoft.AspNetCore.Components;
+
+namespace ECommerce.ClientPortal.ViewModels.Auth;
+
+public class LogoutVM : ProcessingVM
+{
+    private readonly AuthService _authService;
+    private readonly NavigationManager _nav;
+
+    public LogoutVM(AuthService authService, NavigationManager nav)
+    {
+        _authService = authService;
+        _nav = nav;
+    }
+
+    public async Task LogoutAsync()
+    {
+        await RunCommandAsync(() => IsProcessing, async () =>
+        {
+            await _authService.Logout();
+            _nav.NavigateTo("/");
+        });
+    }
+}
