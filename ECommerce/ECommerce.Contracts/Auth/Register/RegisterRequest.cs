309e2f6448210710fe05803ee9b0ef847abfc36a
axel <axdavax93@gmail.com>
2026-04-08 21:16:50 -0400

    Adding DataAnnotations to RegisterRequest

--- ECommerce/ECommerce.Contracts/Auth/Register/RegisterRequest.cs
+++ ECommerce/ECommerce.Contracts/Auth/Register/RegisterRequest.cs
@@ -1,10 +1,19 @@
-﻿namespace ECommerce.Contracts.Auth.Register;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Contracts.Auth.Register;
 
 public class RegisterRequest
 {
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = string.Empty;
+    [Required]
+    [MinLength(6)]
     public string Password { get; set; } = string.Empty;
+    [Compare("Password")]
     public string ConfirmPassword { get; set; } = string.Empty;
+    [Required]
     public string FirstName { get; set; } = string.Empty;
+    [Required]
     public string LastName { get; set; } = string.Empty;
 }
