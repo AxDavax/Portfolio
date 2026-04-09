93e9a13d891e53c668616cdfee899d3c5f36f82b
axel <axdavax93@gmail.com>
2026-04-08 22:18:33 -0400

    Adding UserMenuVM and UserMenu page

---
+++ ECommerce/ECommerce.ClientPortal/ViewModels/Auth/UserMenuVM.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ECommerce.ClientPortal.ViewModels.Auth;
+
+public class UserMenuVM 
+{
+    private readonly NavigationManager _nav;
+
+    public UserMenuVM(NavigationManager nav)
+    {
+        _nav = nav;
+    }
+
+    public void GoToLogin() => _nav.NavigateTo("/login");
+    public void GoToRegister() => _nav.NavigateTo("/register");
+    public void GoToProfile() => _nav.NavigateTo("/profile");
+    public void Logout() => _nav.NavigateTo("/logout");
+}
