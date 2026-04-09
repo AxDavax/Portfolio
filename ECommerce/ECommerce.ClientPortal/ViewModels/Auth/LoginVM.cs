8a9a28c877e86aa60e2321df01344e797587e753
axel <axdavax93@gmail.com>
2026-04-08 21:49:36 -0400

    Adding LoginVM in ViewModels/Auth

---
+++ ECommerce/ECommerce.ClientPortal/ViewModels/Auth/LoginVM.cs
@@ -0,0 +1,38 @@
+﻿using ECommerce.ClientPortal.Services.Auth;
+using ECommerce.ClientPortal.ViewModels.Core;
+using ECommerce.Contracts.Auth.Login;
+using Microsoft.AspNetCore.Components;
+
+namespace ECommerce.ClientPortal.ViewModels.Auth;
+
+public class LoginVM : ProcessingVM
+{
+    public LoginRequest Model { get; set; } = new();
+    public string ErrorMessage { get; set; } = string.Empty;
+
+    public readonly AuthService _authService;
+    public readonly NavigationManager _nav;
+
+    public LoginVM(AuthService authService, NavigationManager nav)
+    {
+        _authService = authService;
+        _nav = nav;
+    }
+
+    public async Task LoginAsync()
+    {
+        await RunCommandAsync(() => IsProcessing, async () =>
+        {
+            ErrorMessage = string.Empty;
+            var success = await _authService.Login(Model);
+
+            if (!success)
+            {
+                ErrorMessage = "Invalid credentials";
+                return;
+            }
+
+            _nav.NavigateTo("/");
+        });
+    }
+}
