b14c6a59d49762260102fd5e70b9ce66f0205b00
axel <axdavax93@gmail.com>
2026-04-09 20:48:21 -0400

    Adding ResetPasswordToken class in Application/Models

---
+++ ECommerce/ECommerce.Application/Models/ResetPasswordToken.cs
@@ -0,0 +1,13 @@
+﻿namespace ECommerce.Application.Models
+{
+    public class ResetPasswordToken
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid UserId { get; set; }
+        public string Token { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime ExpiresAt { get; set; } = DateTime.UtcNow.AddHours(1);
+
+        public bool IsExpired => ExpiresAt <= DateTime.UtcNow;
+    }
+}
