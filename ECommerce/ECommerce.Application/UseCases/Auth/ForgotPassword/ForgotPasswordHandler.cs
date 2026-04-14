7ad728d350e8e588ad9b597006c4205647a4dc6d
axel <axdavax93@gmail.com>
2026-04-13 22:16:14 -0400

    Adjusting ForgotPasswordHandler to use IEmailService implementation

--- ECommerce/ECommerce.Application/UseCases/Auth/ForgotPassword/ForgotPasswordHandler.cs
+++ ECommerce/ECommerce.Application/UseCases/Auth/ForgotPassword/ForgotPasswordHandler.cs
@@ -9,15 +9,16 @@ public class ForgotPasswordHandler
 {
     private readonly IUserRepository _users;
     private readonly IResetPasswordTokenService _resetTokens;
-    //private readonly IEmailService _email;   not done yet
+    private readonly IEmailService _email;   
 
     public ForgotPasswordHandler(
         IUserRepository users,
-        IResetPasswordTokenService resetTokens)
+        IResetPasswordTokenService resetTokens,
+        IEmailService email)
     {
         _users = users;
         _resetTokens = resetTokens;
-        //_email = email;
+        _email = email;
     }
 
     public async Task<ForgotPasswordResponse> HandleAsync(ForgotPasswordRequest request)
@@ -42,9 +43,17 @@ public class ForgotPasswordHandler
             ExpiresAt = DateTime.UtcNow.AddHours(1)
         };
 
-        // 5. Sends the email (for now just mock -> will do it later)
+        // 5. Sends the email 
         var resetLink = $"https://localhost:7083/reset-password?token={token}";
-        Console.WriteLine($"[RESET PASSWORD] Email to {user.Email}: {resetLink}");
+
+        var html = $@"
+            <h2>Password Reset</h2>
+            <p>Click the link below to reset your password:</p>
+            <p><a href=""{resetLink}"">Reset Password</a></p>
+            <p>This link expires in 1 hour.</p>
+        ";
+
+        await _email.SendAsync(user.Email, "Reset your password", html);
 
         // 6. Always Success = true
         return new ForgotPasswordResponse { Success = true };
