a806536b6eb84d4059e0bf89dcfcc7535ebdc4c4
axel <axdavax93@gmail.com>
2026-04-21 13:38:59 -0400

    Refactor of ProductApi

--- ECommerce/ECommerce.ClientPortal/Services/API/Implementations/ProductApi.cs
+++ ECommerce/ECommerce.ClientPortal/Services/API/Implementations/ProductApi.cs
@@ -1,44 +1,25 @@
 ﻿using ECommerce.ClientPortal.Services.API.Interfaces;
 using ECommerce.Contracts.DTO;
-using System.Net.Http.Json;
-
+using Microsoft.AspNetCore.Components;
 namespace ECommerce.ClientPortal.Services.API.Implementations;
 
-public class ProductApi : IProductApi
+public class ProductApi : BaseApi, IProductApi
 {
-    private readonly HttpClient _http;
+    public ProductApi(HttpClient http, NavigationManager nav) : base(http, nav) { }
 
-    public ProductApi(HttpClient http)
-    {
-        _http = http;
-    }
+    public Task<bool> CreateAsync(ProductDTO dto) => SafePost("api/product", dto);
 
-    public async Task<bool> CreateAsync(ProductDTO dto)
-    {
-        var response = await _http.PostAsJsonAsync("api/product", dto);
-        return response.IsSuccessStatusCode;
-    }
-
-    public async Task<bool> DeleteAsync(int id)
-    {
-        var response = await _http.DeleteAsync($"api/product/{id}");
-        return response.IsSuccessStatusCode;
-    }
+    public Task<bool> DeleteAsync(int id) => SafeDelete($"api/product/{id}");
 
     public async Task<List<ProductDTO>> GetAllAsync()
     {
-        var result = await _http.GetFromJsonAsync<List<ProductDTO>>("api/product");
+        var result = await SafeGet<List<ProductDTO>>("api/product");
         return result ?? new List<ProductDTO>();
     }
 
-    public async Task<ProductDTO?> GetByIdAsync(int id)
-    {
-        return await _http.GetFromJsonAsync<ProductDTO>($"api/product/{id}");
-    }
+    public Task<ProductDTO?> GetByIdAsync(int id)
+        => SafeGet<ProductDTO?>($"api/product/{id}");
 
-    public async Task<bool> UpdateAsync(int id, ProductDTO dto)
-    {
-        var response = await _http.PutAsJsonAsync($"api/product/{id}", dto);
-        return response.IsSuccessStatusCode;
-    }
+    public Task<bool> UpdateAsync(int id, ProductDTO dto)
+        => SafePut($"api/product/{id}", dto);
 }
