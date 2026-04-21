f2594477bd1d2c1e0369ecc4a0817028e51654cc
axel <axdavax93@gmail.com>
2026-04-21 12:59:16 -0400

    Creating a BaseApi to handle API request and errors so services API can derived from

---
+++ ECommerce/ECommerce.ClientPortal/Services/API/Implementations/BaseApi.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace ECommerce.ClientPortal.Services.API.Implementations;
+
+public abstract class BaseApi
+{
+    protected readonly HttpClient _http;
+    protected readonly NavigationManager _navigation;
+
+    protected BaseApi(HttpClient http, NavigationManager navigation)
+    {
+        _http = http;
+        _navigation = navigation;
+    }
+
+    protected async Task<T?> SafeGet<T>(string url)
+    {
+        try
+        {
+            return await _http.GetFromJsonAsync<T>(url);
+        }
+        catch (HttpRequestException ex)
+        {
+            HandleHttpError(ex.StatusCode);
+            return default;
+        }
+        catch
+        {
+            _navigation.NavigateTo("/error", true);
+            return default;
+        }
+    }
+
+    protected async Task<bool> SafePost(string url, object? body = null)
+    {
+        try
+        {
+            var response = await _http.PostAsJsonAsync(url, body);
+            if (!response.IsSuccessStatusCode)
+                HandleHttpError(response.StatusCode);
+
+            return response.IsSuccessStatusCode;
+        }
+        catch (HttpRequestException ex)
+        {
+            HandleHttpError(ex.StatusCode);
+            return false;
+        }
+        catch
+        {
+            _navigation.NavigateTo("/error", true);
+            return false;
+        }
+    }
+
+    protected async Task<bool> SafePut(string url, object? body = null)
+    {
+        try
+        {
+            var response = await _http.PutAsJsonAsync(url, body);
+            if (!response.IsSuccessStatusCode)
+                HandleHttpError(response.StatusCode);
+
+            return response.IsSuccessStatusCode;
+        }
+        catch (HttpRequestException ex)
+        {
+            HandleHttpError(ex.StatusCode);
+            return false;
+        }
+        catch
+        {
+            _navigation.NavigateTo("/error", true);
+            return false;
+        }
+    }
+
+    protected async Task<bool> SafeDelete(string url)
+    {
+        try
+        {
+            var response = await _http.DeleteAsync(url);
+            if (!response.IsSuccessStatusCode)
+                HandleHttpError(response.StatusCode);
+
+            return response.IsSuccessStatusCode;
+        }
+        catch (HttpRequestException ex)
+        {
+            HandleHttpError(ex.StatusCode);
+            return false;
+        }
+        catch
+        {
+            _navigation.NavigateTo("/error", true);
+            return false;
+        }
+    }
+
+    private void HandleHttpError(HttpStatusCode? status)
+    {
+        switch (status)
+        {
+            case HttpStatusCode.Unauthorized:
+                _navigation.NavigateTo("/Auth/Login", true);
+                break;
+
+            case HttpStatusCode.Forbidden:
+                _navigation.NavigateTo("/access-denied", true);
+                break;
+
+            case HttpStatusCode.NotFound:
+                _navigation.NavigateTo("/404", true);
+                break;
+
+            default:
+                _navigation.NavigateTo("/error", true);
+                break;
+        }
+    }
+}
