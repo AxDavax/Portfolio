b1e43fa81a9118bcb5acc6d4a7139b1e692325f1
axel <axdavax93@gmail.com>
2026-04-22 00:14:08 -0400

    Adding IUserRoleService and his implementation

---
+++ ECommerce/ECommerce.Application/Interfaces/IUserRoleService.cs
@@ -0,0 +1,6 @@
+﻿namespace ECommerce.Application.Interfaces;
+
+public interface IUserRoleService
+{
+    Task AssignRoleAsync(Guid userId, Guid roleId);
+}
---
+++ ECommerce/ECommerce.Infrastructure/Auth/UserRoleService.cs
@@ -0,0 +1,27 @@
+﻿using ECommerce.Application.Interfaces;
+using ECommerce.Domain.Interfaces;
+
+namespace ECommerce.Infrastructure.Auth
+{
+    public class UserRoleService : IUserRoleService
+    {
+        private readonly ISqlDataAccess _db;
+
+        public UserRoleService(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task AssignRoleAsync(Guid userId, Guid roleId)
+        {
+            const string sql = """
+                INSERT INTO 
+                    UserRoles (UserId, RoleId)
+                VALUES 
+                    (@UserId, @RoleId)
+            """;
+
+            return _db.ExecuteAsync(sql, new { UserId = userId, RoleId = roleId });
+        }
+    }
+}
