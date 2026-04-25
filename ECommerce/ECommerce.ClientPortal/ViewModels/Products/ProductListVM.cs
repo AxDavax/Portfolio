97bcd38aebc6ce6e8c7e91f6084769bd9714aee1
axel <axdavax93@gmail.com>
2026-04-25 12:41:53 -0400

    feat(product): add ProductListVM with product loading and delete confirmation workflow

---
+++ ECommerce/ECommerce.ClientPortal/ViewModels/Products/ProductListVM.cs
@@ -0,0 +1,77 @@
+﻿using ECommerce.ClientPortal.Services.API.Interfaces;
+using ECommerce.ClientPortal.Services.Extensions;
+using ECommerce.ClientPortal.ViewModels.Core;
+using ECommerce.Contracts.DTO;
+using Microsoft.JSInterop;
+
+namespace ECommerce.ClientPortal.ViewModels.Products;
+
+public class ProductListVM : ProcessingVM
+{
+    private readonly IProductApi _productApi;
+    private readonly IJSRuntime _js;
+
+    public ProductListVM(IProductApi productApi, IJSRuntime js)
+    {
+        _productApi = productApi;
+        _js = js;
+    }
+
+    private IEnumerable<ProductDTO> _products = new List<ProductDTO>();
+    public IEnumerable<ProductDTO> Products
+    {
+        get => _products;
+        set => SetProperty(ref _products, value);
+    }
+
+    private int _deleteProductID;
+    public int DeleteProductID
+    {
+        get => _deleteProductID;
+        set => SetProperty(ref _deleteProductID, value);
+    }
+
+    public async Task AfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await LoadProductsAsync();
+        }
+    }
+
+    private async Task LoadProductsAsync()
+    {
+        await RunCommandAsync(() => IsProcessing, async () => 
+        {
+            Products = await _productApi.GetAllAsync();
+        });
+    }
+
+    public void HandleDelete(int id)
+    {
+        DeleteProductID = id;
+        _js.InvokeVoidAsync("ShowConfirmationModal");
+    }
+
+    public async Task ConfirmDeleteAsync(bool isConfirmed)
+    {
+        await RunCommandAsync(() => IsProcessing, async () =>
+        {
+            await _js.InvokeVoidAsync("HideConfirmationModal");
+
+            if (isConfirmed && DeleteProductID != 0)
+            {
+                var result = await _productApi.DeleteAsync(DeleteProductID);
+
+                if (result)
+                    _js?.ToastrSuccess("Product deleted successfully");
+                else
+                    _js?.ToastrError("Error Encountered while deleting");
+
+                await LoadProductsAsync();
+            }
+
+            DeleteProductID = 0;
+        });
+    }
+}
