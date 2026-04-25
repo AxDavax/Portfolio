ceb7035fa2b90138195cc39df454ddec72001e0f
axel <axdavax93@gmail.com>
2026-04-25 11:23:26 -0400

    feat(category): add CategoryListVM with CRUD logic

---
+++ ECommerce/ECommerce.ClientPortal/ViewModels/Categories/CategoryListVM.cs
@@ -0,0 +1,83 @@
+﻿using ECommerce.ClientPortal.Services.API.Interfaces;
+using ECommerce.ClientPortal.Services.Extensions;
+using ECommerce.ClientPortal.ViewModels.Core;
+using ECommerce.Contracts.DTO;
+using Microsoft.JSInterop;
+
+namespace Portfolio.ECommerce.Blazor.ViewModels.Categories
+{
+    public class CategoryListVM : ProcessingVM
+    {
+        private readonly ICategoryApi _categoryApi;
+        private readonly IJSRuntime _js;
+
+        public CategoryListVM(ICategoryApi categoryApi, IJSRuntime js)
+        {
+            _categoryApi = categoryApi;
+            _js = js;
+        }
+
+        private IEnumerable<CategoryDTO> _categories = new List<CategoryDTO>();
+        public IEnumerable<CategoryDTO> Categories
+        {
+            get => _categories;
+            set => SetProperty(ref _categories, value);
+        }
+
+        private int _deleteCategoryID;
+        public int DeleteCategoryID
+        {
+            get => _deleteCategoryID;
+            set => SetProperty(ref _deleteCategoryID, value);
+        }
+
+        public async Task InitializeAsync()
+        {
+            await Task.Delay(5000);
+        }
+
+        public async Task AfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await LoadCategoriesAsync();
+            }
+        }
+
+        public async Task LoadCategoriesAsync()
+        {
+            await RunCommandAsync(() => IsProcessing, async () =>
+            {
+                Categories = await _categoryApi.GetAllAsync();
+            });
+        }
+
+        public void HandleDelete(int id)
+        {
+            DeleteCategoryID = id;
+            _js.InvokeVoidAsync("ShowConfirmationModal");
+        }
+
+        public async Task ConfirmDeleteAsync(bool isConfirmed)
+        {
+            await RunCommandAsync(() => IsProcessing, async () =>
+            {
+                await _js.InvokeVoidAsync("HideConfirmationModal");
+
+                if (isConfirmed && DeleteCategoryID != 0)
+                {
+                    var result = await _categoryApi.DeleteAsync(DeleteCategoryID);
+
+                    if (result)
+                        _js?.ToastrSuccess("Category deleted successfully");
+                    else
+                        _js?.ToastrError("Error deleting category");
+
+                    await LoadCategoriesAsync();
+                }
+
+                DeleteCategoryID = 0;
+            });
+        }
+    }
+}
