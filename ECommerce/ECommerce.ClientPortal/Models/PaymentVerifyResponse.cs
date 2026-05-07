1b779fec5df1b932d49ec3b8babd64af67e2bb01
axel <axdavax93@gmail.com>
2026-05-06 20:36:16 -0400

    feat(contracts): add PaymentVerifyResponse with Success, Order and Message fields
    Moved PaymentVerifyResponse from ECommerce.ClientPortal/Models to ECommerce.Contracts/DTO/Payment so it can be shared
    between backend and client. Added Order (OrderHeaderDTO?) and Message
    (string) fields to support the new payment verification workflow.

--- ECommerce/ECommerce.ClientPortal/Models/PaymentVerifyResponse.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ECommerce.ClientPortal.Models;
-
-public class PaymentVerifyResponse
-{
-    public bool Success { get; set; }
-}
---
+++ ECommerce/ECommerce.Contracts/DTO/Payment/PaymentVerifyResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.Contracts.DTO.Payment;
+
+public class PaymentVerifyResponse
+{
+    public bool Success { get; set; }
+    public OrderHeaderDTO? Order { get; set; }
+    public string Message { get; set; } = string.Empty;
+}
