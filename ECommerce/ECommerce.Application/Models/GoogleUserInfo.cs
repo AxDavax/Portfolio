f21b395c77c9018b4c3cdad5167c46b2ba58a7bc
axel <axdavax93@gmail.com>
2026-05-11 15:15:27 -0400

    feat(auth): add GoogleUserInfo model for deserializing Google userinfo response
    Introduced GoogleUserInfo to map the minimal fields required from Google's
    OpenID Connect userinfo endpoint (sub, email).

---
+++ ECommerce/ECommerce.Application/Models/GoogleUserInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace ECommerce.Application.Models;
+
+public class GoogleUserInfo
+{
+    public string Sub { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
